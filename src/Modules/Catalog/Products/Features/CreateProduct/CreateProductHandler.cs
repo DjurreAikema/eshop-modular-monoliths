3b7ab60b652706bd569927e7368d694920a7829d
d.aikema <djurreaikema@gmail.com>
2025-12-16 14:14:24 +0100

    Course 2 - Module 1 - Application use case development with CQRS and MediatR

---
+++ src/Modules/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand(string name, List<string> categories, string description, string imageFile, decimal price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
