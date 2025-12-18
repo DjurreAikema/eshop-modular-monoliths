d3385cdf8d798142c72b513157bdd7fd8586acae
d.aikema <djurreaikema@gmail.com>
2025-12-18 16:09:14 +0100

    Course 2 - Module 5 - Develop Basket module with vertical slice architecture and CQRS

---
+++ src/Modules/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,27 @@
+﻿using Shared.DDD;
+
+namespace Basket.Basket.Models;
+
+public class ShoppingCartItem : Entity<Guid>
+{
+    // --- Properties
+    public Guid ShoppingCartId { get; private set; } = Guid.Empty;
+    public Guid ProductId { get; private set; } = Guid.Empty;
+    public int Quantity { get; internal set; } = 0;
+    public string Color { get; private set; } = null!;
+
+    // Comes from Catalog module
+    public decimal Price { get; private set; } = 0;
+    public string ProductName { get; private set; } = null!;
+
+    // --- Methods
+    internal ShoppingCartItem(Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+    {
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+}
